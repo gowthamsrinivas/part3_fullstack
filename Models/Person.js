49d1829053bc1731effa292db92db2df089abecd
GowthamSrinivasanNN <gowthams@niagaranetworks.com>
2020-12-01 17:03:58 +0530

    uibuild

--- Models/Person.js
+++ Models/Person.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(result => {
-    console.log('Connected to MongoDB');
-})
-.catch(error => {
+  .then(result => {
+    console.log(`Connected to MongoDB ${result}`);
+  })
+  .catch(error => {
     console.log(`error connecting to mongoDB ${error.message}`);
-})
+  });
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true, minlength: 3 },
-  date: {type: Date, required: true},
-  number: {type: String, minlength: 8},
+  date: { type: Date, required: true },
+  number: { type: String, minlength: 8 },
 });
 
 personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
 module.exports = mongoose.model('Person', personSchema);
--- index.js
+++ index.js
@@ -9,100 +9,100 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
-morgan.token('body',function(req,res) {
-    return JSON.stringify(req.body);
+morgan.token('body',function(req) {
+  return JSON.stringify(req.body);
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons',(req,res) => {
-    Person.find({}).then(result => {
-        console.log("Getting Phonebook details");
-        res.json(result);
-    })
+  Person.find({}).then(result => {
+    console.log('Getting Phonebook details');
+    res.json(result);
+  });
 });
 
 app.get('/api/persons/:id',(req,res,next) => {
-    Person.findById(req.params.id).then(result => {
-        if(!result) {
-            res.status('404').end();
-        }
-        else {
-            res.json(result);
-        }
-    })
+  Person.findById(req.params.id).then(result => {
+    if(!result) {
+      res.status('404').end();
+    }
+    else {
+      res.json(result);
+    }
+  })
     .catch(error => {
-        next(error);
+      next(error);
     });
 });
 
 app.get('/info',(req,res) => {
-    Person.count({}).then(totalItems => {
-        res.send(`<div>Phonebook has info for ${totalItems} people</div>
+  Person.count({}).then(totalItems => {
+    res.send(`<div>Phonebook has info for ${totalItems} people</div>
         <div>${new Date()}</div>`);
-    });
+  });
 });
 
 app.post('/api/persons',(req,res,next) => {
-    const body = req.body;
-    if(!body.name || !body.number) {
-        return res.status('400').json({error: 'Content Missing'});
-    }
+  const body = req.body;
+  if(!body.name || !body.number) {
+    return res.status('400').json({ error: 'Content Missing' });
+  }
 
-    const person = new Person({
-        name:body.name,
-        number:body.number,
-        date: new Date()
-    });
+  const person = new Person({
+    name:body.name,
+    number:body.number,
+    date: new Date()
+  });
 
-    person.save().then(result => {
-        console.log(`Phonebook entry added ${result}`);
-        res.json(result);
-    })
+  person.save().then(result => {
+    console.log(`Phonebook entry added ${result}`);
+    res.json(result);
+  })
     .catch(error => next(error));
 });
 
 app.put('/api/persons/:id',(req,res,next) => {
-    const body = req.body;
+  const body = req.body;
 
-    const person = {
-        name:body.name,
-        number:body.number
-    };
+  const person = {
+    name:body.name,
+    number:body.number
+  };
 
-    Person.findByIdAndUpdate(req.params.id,person,{new: true}).then(result => {
-        console.log(`Phonebook entry updated ${result}`);
-        res.json(result);
-    })
+  Person.findByIdAndUpdate(req.params.id,person,{ new: true }).then(result => {
+    console.log(`Phonebook entry updated ${result}`);
+    res.json(result);
+  })
     .catch(error => {
-        next(error);
+      next(error);
     });
 });
 
 app.delete('/api/persons/:id',(req,res) => {
-    Person.findOneAndDelete(req.params.id).then(result => {
-        if(result) {
-            res.status('204').end();
-        }
-        else {
-            res.status('404').json({error:'Content not found'});
-        }
-    });
+  Person.findOneAndDelete(req.params.id).then(result => {
+    if(result) {
+      res.status('204').end();
+    }
+    else {
+      res.status('404').json({ error:'Content not found' });
+    }
+  });
 });
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message });
-    }
-    next(error);
-}
-  
-  app.use(errorHandler)
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+  next(error);
+};
 
+app.use(errorHandler);
+
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
